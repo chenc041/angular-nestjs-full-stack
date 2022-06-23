9ecc41919153ebe05c96f33dfebc321c2a7758df
chenc <double_cl@163.com>
2022-06-23 12:31:13 +0800

    chore: replace base code

---
+++ api/src/config/winston-config/winston-config.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import * as winston from 'winston';
+import 'winston-daily-rotate-file';
+import {
+  WinstonModuleOptionsFactory,
+  WinstonModuleOptions,
+} from 'nest-winston';
+import * as pkg from '../../../package.json';
+
+const { json, timestamp, combine } = winston.format;
+
+@Injectable()
+export class WinstonConfigService implements WinstonModuleOptionsFactory {
+  createWinstonModuleOptions(): WinstonModuleOptions {
+    const { NODE_ENV } = process.env;
+    const transports: any[] = [];
+    if (NODE_ENV === 'development') {
+      transports.push(new winston.transports.Console());
+    } else {
+      const fileInfoTransport = new winston.transports.DailyRotateFile({
+        level: 'info',
+        maxSize: '5m',
+        maxFiles: '14d',
+        zippedArchive: true,
+        datePattern: 'YYYY-MM-DD',
+        filename: `log/${pkg.name}-info-%DATE%.log`,
+      });
+
+      const fileErrorTransport = new winston.transports.DailyRotateFile({
+        level: 'error',
+        maxSize: '5m',
+        maxFiles: '14d',
+        zippedArchive: true,
+        datePattern: 'YYYY-MM-DD',
+        filename: `log/${pkg.name}-error-%DATE%.log`,
+      });
+      transports.push(fileInfoTransport);
+      transports.push(fileErrorTransport);
+    }
+    return {
+      transports: transports,
+      format: combine(timestamp(), json()),
+      defaultMeta: {
+        appName: pkg.name,
+      },
+    };
+  }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+## angular and nestjs full stack starter
