28995dd0c36ad7feb78ebbc6eece12bca5a1c6e7
chenc <double_cl@163.com>
2022-06-24 13:47:15 +0800

    chore: clean up code

--- packages/webapp/src/app/app-routing.module.ts
+++ packages/webapp/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { environment } from '~/environments/environment';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { enableTracing: !environment.production })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
