fc3da17de74cfdf22e67fd66127db77ff2e5c836
chenc <double_cl@163.com>
2022-07-09 15:23:47 +0800

    chore: add test example (#4)
    * chore: add test example
    
    * chore: update dep
    
    * chore: complete test config

--- packages/api/src/config/typeorm-config/typeorm-config.service.ts
+++ packages/api/src/config/typeorm-config/typeorm-config.service.ts
@@ -2,21 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 import { join } from 'path';
-import * as pkg from 'package.json';
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   constructor(private readonly configService: ConfigService) {}
 
-  createTypeOrmOptions(
-    connectionName?: string,
-  ): Promise<TypeOrmModuleOptions> | TypeOrmModuleOptions {
+  createTypeOrmOptions(connectionName?: string): Promise<TypeOrmModuleOptions> | TypeOrmModuleOptions {
     return {
       type: 'mysql',
       timezone: '+08:00',
-      name: connectionName || pkg.name,
+      name: connectionName || 'api',
       host: this.configService.get('DB_HOST'),
-      port: Number(this.configService.get('DB_PORT')),
+      port: +this.configService.get('DB_PORT'),
       username: this.configService.get('DB_USERNAME'),
       password: this.configService.get('DB_PASSWORD'),
       database: this.configService.get('DB_NAME'),
