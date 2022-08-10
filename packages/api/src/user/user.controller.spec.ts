d1db491a26432ba584ace4383bf53feccac9a829
chenc <double_cl@163.com>
2022-08-10 18:10:04 +0800

    feat: update e2e test case

--- packages/api/src/user/user.controller.spec.ts
+++ packages/api/src/user/user.controller.spec.ts
@@ -1,13 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { TypeOrmTestingModule } from '~/test-utils';
+import { UserService } from '~/user/user.service';
+import { UserEntity } from '~/entities/user.entity';
 
 describe('UserController', () => {
   let controller: UserController;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController],
-    }).compile();
+    const module: TestingModule = await Test.createTestingModule(
+      TypeOrmTestingModule({
+        controllers: [UserController],
+        providers: [UserService],
+        entities: [UserEntity],
+      }),
+    ).compile();
 
     controller = module.get<UserController>(UserController);
   });
--- packages/api/src/user/user.service.spec.ts
+++ packages/api/src/user/user.service.spec.ts
@@ -1,13 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { TypeOrmTestingModule } from '~/test-utils';
+import { UserController } from '~/user/user.controller';
+import { UserEntity } from '~/entities/user.entity';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
-    }).compile();
+    const module: TestingModule = await Test.createTestingModule(
+      TypeOrmTestingModule({
+        controllers: [UserController],
+        providers: [UserService],
+        entities: [UserEntity],
+      }),
+    ).compile();
 
     service = module.get<UserService>(UserService);
   });
