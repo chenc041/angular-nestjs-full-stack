bc4f2cf7ae807f6c6591e8eace3499fadd838299
chenc <double_cl@163.com>
2023-11-16 16:35:15 +0800

    feat: login page

--- packages/webapp/src/app/pages/login/login.component.ts
+++ packages/webapp/src/app/pages/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
-
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NzFormModule, NzInputModule, NzButtonModule],
   templateUrl: './login.component.html',
 })
 export default class LoginComponent implements OnInit {
@@ -13,6 +15,19 @@ export default class LoginComponent implements OnInit {
   validateForm!: UntypedFormGroup;
 
   constructor(private fb: UntypedFormBuilder) {
+  }
+
+  submitForm(): void {
+    if (this.validateForm.valid) {
+      console.log('submit', this.validateForm.value);
+    } else {
+      Object.values(this.validateForm.controls).forEach((control) => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
   }
 
   ngOnInit(): void {
